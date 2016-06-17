badd9807909c78dc08165f709a071b58f03518c7
rafacrz <rafael.felipe1989@gmail.com>
2016-06-17 11:47:26 -0300

    comentários adicionados e alterados

--- FormUtil.java
+++ FormUtil.java
@@ -1,5 +1,8 @@
-/*
- *
+/**
+ * @author Rafael Felipe
+ * @e-mail rafael.felipe1989@gmail.com
+ * @version 1.0
+ * @since 12/06/2016
  */
 package form_util;
 
@@ -9,34 +12,25 @@ import javax.swing.JOptionPane;
 import javax.swing.text.JTextComponent;
 
 /**
- *
- * @author Rafael Felipe
- * @e-mail rafael.felipe1989@gmail.com
- * @version 1.0
- * @since 12/06/2016
+ * Essa classe contém vários métodos úteis na criação de formulários em Java.
  */
 public class FormUtil {
 
-    //Mesage for input nulls
-    private static final String OBRIGATORIO = "Campo obrigatório";
-
-    private static final String CLEAN = "";
+    private static final String CLEAN = ""; //campo vazio
 
     /**
-     * This method check the inputs
+     * Verifica se os campos de texto passados por parametros estão preenchidos
      *
-     * @param inputs JTextField's inputs
-     * @return <b>true</b> - All inputs are complete <br> <b>false</b> - The are
-     * inputs void
+     * @param mensagem Mensagem que será exibida no campo vazio alertando o usuário
+     * @param inputs campos de texto JTextField
+     * @return <b>true</b> - Todos os campos estão preenchidos <br> <b>false</b> - Existe pelo menos um campo vazio
      */
-    public static boolean isFull(JTextComponent... inputs) {
+    public static boolean isFull(String mensagem, JTextComponent... inputs) {
         boolean status = true;
         for (JTextComponent input : inputs) {
-            if (input.getText().equalsIgnoreCase(CLEAN) || input.getText().equalsIgnoreCase(OBRIGATORIO)) {
+            if (input.getText().equalsIgnoreCase(CLEAN) || input.getText().equalsIgnoreCase(mensagem)) {
                 input.setBackground(Color.red);
-                input.setText(OBRIGATORIO);
-                // input.setfocus
-                //input.setToolTipText(OBRIGATORIO);
+                input.setText(mensagem);
                 status = false;
             } else {
                 input.setBackground(Color.WHITE);
@@ -47,9 +41,9 @@ public class FormUtil {
     }
 
     /**
-     * This method clear the inputs of the param
+     * Limpa todos os campos de texto passados por parâmetro
      *
-     * @param inputs
+     * @param inputs campos de texto JTextField
      */
     public static void clearInputs(JTextComponent... inputs) {
         for (JTextComponent input : inputs) {
@@ -59,10 +53,11 @@ public class FormUtil {
             }
         }
     }
-    
+
     /**
-     * This method enable the components get the param
-     * @param components 
+     * Habilita todos os componentes (JComponent) passados por parâmetro
+     *
+     * @param components componentes JComponent 
      */
     public static void enableComponents(JComponent... components) {
         for (JComponent component : components) {
@@ -71,10 +66,11 @@ public class FormUtil {
             }
         }
     }
-    
+
     /**
-     * This method disable the components get the param
-     * @param components 
+     * Desabilita todos os componentes (JComponent) passados por parâmetro
+     *
+     * @param components components JComponent
      */
     public static void disableComponents(JComponent... components) {
         for (JComponent component : components) {
@@ -83,11 +79,13 @@ public class FormUtil {
             }
         }
     }
-    
-    public static void msg(String txt){
+
+    /**
+     * Exibe uma mensagem de texto (JOptionPane)
+     * @param txt mensagem de texto
+     */
+    public static void msg(String txt) {
         JOptionPane.showConfirmDialog(null, txt);
     }
-
-
 
 }
